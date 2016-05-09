813626846aea387ae7b2304b330218d95a1f86a5
armando <jorgearmandou@gmail.com>
2016-05-08 23:18:18 -0500

    estilos a actualizar cuotas

--- pags/cambiarContrasena.php
+++ pags/cambiarContrasena.php
@@ -2,9 +2,36 @@
 <html lang="es">
    <head>
        <meta charset="utf-8">
+       <meta name="language" content="ES">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+        <meta name="description" content="apuestas san juan de pasto,bookiesport,apuestas de futbol, nariño colombia apuestas"/>
+        <meta name="keywords" content="sitio para hacer apuestas,bookiesport, apuestas de futbol, san juan de pasto apuestas"/>
+        <meta name="author" content="Reon-Soluciones_Web"/>
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
        <title>cambiarContraseña</title>
+       <link rel="stylesheet" href="../css/normailze.min.css">
+        <link rel="stylesheet" href="../css/estilosform.css">
    </head>
     <body>
+     <div id='contenedor'>
+          <header id="cabecera">
+             <div id="logo">
+                 
+             </div>
+                 <ul id="cabecera">
+                     <li class="logoutico">
+                       <a href="salir.php">
+                           <img src="../images/Bookiesport_Usuario.png" alt="usuario"></a>
+                     </li>
+                     <li class="logout">
+                         <a href="salir.php">Salir</a>
+                     </li>
+                     <li id="salirico">
+                         <a href="administrador.php"><img src='../images/Bookiesport_Inicio.png' alt="Inicio"></a>
+                     </li>
+                 </ul>
+          </header>
+          <div id="contenido">
     <?php
         require_once('gestionDB.php');
         
@@ -12,10 +39,10 @@
         $idusuario=$_SESSION['id'];
         $user = $_SESSION['usuario'];
     ?>
-    <div id="contenido">
+    <center>
     <h2>Cambio de contraseña</h2>
     <p>
-        A continuación podra cambiar la contraseña para el usuario <?php echo $user; ?>
+        A continuación podra cambiar la contraseña para el usuario <strong><?php echo $user; ?></strong>
     </p>
        <form method="post" id="formcontrasena" action="lib/updateContrasena.php">
         <ul>
@@ -36,8 +63,18 @@
             </li>
         </ul>
         </form>
-        
-        <a href="administrador.php">Inicio</a>
+        </center>
+         </div>
+         <footer>
+              <img src="../images/Bookiesport_Logo.png">
+              <p>
+                  BOOKIESPORT<br>
+                  empresa dedicada a las apuestas
+                  visitanos en 
+                  tel:
+                  
+              </p>
+          </footer>
         </div>
     </body>
     <script src="http://code.jquery.com/jquery-2.2.0.min.js"></script>
--- pags/lib/cmbPartidos.php
+++ pags/lib/cmbPartidos.php
@@ -4,7 +4,11 @@ require_once '../gestionDB.php';
 $fecha = strip_tags($_POST['fecha']);
 $enlace = connectionDB();
 $partidos = partidos($enlace,$fecha);
- $res="<h2>Listado de partidos</h2>";
+ $res="<h2>Listado de partidos</h2>
+ <table><tr><th><label>partido - hora</label></th>
+ <th><label> 1 </label></th>
+ <th><label> X </label></th>
+ <th><label> 2 </label></th>";
 //$res.= "<option value='--seleccion--'>--seleccione partido--</option>";
 //0,0 id, 0,1 local, 0,2 visitante, 0,3 liga, 0,4 hora
 for($i=0;$i<count($partidos);$i++){
@@ -19,10 +23,24 @@ for($i=0;$i<count($partidos);$i++){
     $nomLiga = nomLiga($nomLiga,$enlace);
         
         $res.="<li><form method='POST' id='miniformulario' action='lib/cuotas.php'>
-        <input type='text' name='id' value='".$idP."' class='ids'><label'>".$nomEquiA." VS ".$nomEquiB." - ".$hora." : </label><label> 1: </label><input type='number' step='any' class='cuotas' value='".$local."' name='cuota1' min='0'><label> X: </label><input type='number' step='any' class='cuotas' value='".$empate."' name='cuotax' min='0'><label> 2: </label><input type='number' step='any' class='cuotas' value='".$visitante."'  name='cuota2' min='0'><button type='input'>ACTUALIZAR</button></form></li>";
+        
+        <input type='text' name='id' value='".$idP."' class='ids'>
+        <tr><td class='tdpartido'>
+        <label>".$nomEquiA."_ vs _ ".$nomEquiB." - ".$hora." </label></td>
+
+        <td><input type='number' step='any' class='cuotas' value='".$local."' name='cuota1' min='0'></td>
+
+        <td><input type='number' step='any' class='cuotas' value='".$empate."' name='cuotax' min='0'></td>
+
+        <td><input type='number' step='any' class='cuotas' value='".$visitante."'  name='cuota2' min='0'></td>
+        <td><button type='input'>ACTUALIZAR</button></td></tr>
+        </form></li>";
 }
 connectionClose($enlace);
-
+$res.="</table>";
 echo $res;
 
 ?>
+        
+        
+        
