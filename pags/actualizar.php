ddd174d46ba4ef284881f91ae411d09bb576dc3c
Edison <easrevelo@gmail.com>
2016-05-11 02:14:08 -0500

    equipo ganador

---
+++ pags/actualizar.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once('gestionDB.php');
+require_once('validaciones.php');
+
+$idp = $_REQUEST['idpartido'];
+
+$arr = array();
+$ganador=strip_tags($_POST['equipo']);
+$enlace = connectionDB();
+$arr=equiposLigaPartido($enlace,$idp);
+
+mysqli_query($enlace,"UPDATE partidos set GANADOR='$ganador' where ID='$idp'")
+            or die("error al actualizar");
+connectionClose($enlace);
+?>
--- pags/lib/llenarcomboapuesta.php
+++ pags/lib/llenarcomboapuesta.php
@@ -10,11 +10,10 @@ $arr=equiposLigaPartido($enlace,$idp);
 if($arr){
     $idEquipoA=$arr[0];
     $idEquipoB=$arr[1];
-    $nomEquiA = nomEquipo($idEquipoA,$enlace);
-    $nomEquiB = nomEquipo($idEquipoB,$enlace);
+    
 }
 connectionClose($enlace);
-echo("<option value='seleccion'>selecione equipo</option><option value='".$nomEquiA."'>".$nomEquiA."</option>
-<option value='".$nomEquiB."'>".$nomEquiB."</option><option value='X'>Enpate</option>");
+echo("<option value='seleccion'>selecione equipo</option><option value='".$idEquipoA."'>".$idEquipoA."</option>
+<option value='".$idEquipoB."'>".$idEquipoB."</option><option value='Empate'>Empate</option>");
 
 ?>
