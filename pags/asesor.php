0b7527aa9891aa6fdfe45dc7fa3f07f14a862a21
Jorge urbina <jorgearmandou@gmail.com>
2016-05-11 14:54:07 -0500

    work in apuesta

--- pags/asesor.php
+++ pags/asesor.php
@@ -10,7 +10,6 @@
    </head>
     <body>
         <h1>bienbenidos asesores</h1>
-        <a href='apuestas.php'>Realizar apuesta</a><br>
         <a href='cambiarContrasena.php'>Cambiar contraseña</a><br>
         <a href='apuesta.php'>apuestas</a><br>
         <a href='apuestaspersona.php'>total apuestas</a><br>
